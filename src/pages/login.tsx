0ff41e8325469592c75156b9976e0fd07dee5608
exesalle <exesalle@gmail.com>
2023-03-31 11:22:57 +0300

    .

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SignIn from '../components/SignIn';
+import {Link} from 'react-router-dom';
 
 const Login = () => {
   return (
     <>
       <SignIn/>
+      <p>Нет аккаунта?<Link to="/registration">Зарегистрируйтесь</Link></p>
     </>
   );
 };
--- src/pages/registration.tsx
+++ src/pages/registration.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import SignUp from '../components/SignUp';
+import {Link} from 'react-router-dom';
 
 const Registration = () => {
   return (
-    <>
+    <><div className="App">
       <SignUp/>
+      <p>
+        У вас уже есть аккаунт? <Link to="/login">Войти</Link>
+      </p>
+    </div>
     </>
   );
 };
