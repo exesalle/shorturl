1a1713f92df69a646cbe0d122d654defc6d5af54
exesalle <exesalle@gmail.com>
2023-04-07 12:58:18 +0300

    .

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {IShortedLinks} from '../Types';
 import {Button, Input, Space} from 'antd';
-import {useParams} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
 import {RootState} from '../store/store';
 import {getLinks, addLink, deleteLink, updateHash, updateLink} from '../thunks';
